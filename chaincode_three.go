b37a046e5616f6db96bd5ea3cd994952f98339cf
DeepalakshmiPerumal <athulyasakthi@gmail.com>
2017-05-05 16:29:19 +0530

    Update chaincode_three.go

--- chaincode_three.go
+++ chaincode_three.go
@@ -75,7 +75,8 @@ func (t *SimpleChaincode) Query(stub shim.ChaincodeStubInterface, function strin
 
 	// Handle different functions
 	if function == "read" { //read a variable
-		return t.read(stub, args)
+		fmt.Println("query is running " + function)
+		//return t.read(stub, args)
 	}
 	fmt.Println("query did not find func: " + function)
 
