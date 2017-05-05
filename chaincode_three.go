9190986b545df6721f211d6d8f73a4546810fa2c
DeepalakshmiPerumal <athulyasakthi@gmail.com>
2017-05-05 20:34:00 +0530

    Update chaincode_three.go

--- chaincode_three.go
+++ chaincode_three.go
@@ -119,3 +119,25 @@ func (t *SimpleChaincode) write(stub shim.ChaincodeStubInterface, args []string)
 	//}
 	return nil, nil
 }
+func (t *SimpleChaincode) read(stub shim.ChaincodeStubInterface, args []string) ([]byte, error) {
+	var key, jsonResp string
+	var err error
+	var empJSON employee
+
+	if len(args) != 1 {
+		return nil, errors.New("Incorrect number of arguments. Expecting name of the key to query")
+	}
+
+	key = args[0]
+	valAsbytes, err := stub.GetState(key)
+	if err != nil {
+		jsonResp = "{\"Error\":\"Failed to get state for " + key + "\"}"
+		return nil, errors.New(jsonResp)
+	}
+	err = json.Unmarshal([]byte(valAsbytes), &empJSON)
+	if err != nil {
+		jsonResp = "{\"Error\":\"Failed to decode JSON of: " + key + "\"}"
+		return nil, err
+	}
+	return valAsbytes, nil
+}
